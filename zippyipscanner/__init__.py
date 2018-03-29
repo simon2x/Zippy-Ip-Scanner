0093eb41af66a89f319f8cb051e93854b708bcbc
swprojects <swprojects@gmx.com>
2018-03-29 13:14:58 +0100

    Allow import version from root dir

--- zippyipscanner/__init__.py
+++ zippyipscanner/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from ._version import get_versions
 
-__author__ = 'Simon Wu'
-__email__ = 'swprojects@runbox.com'
-__version__ = get_versions()['version']
-del get_versions
-
+try:
+    from version import __version__
+except:
+    from zippyipscanner.version import __version__
