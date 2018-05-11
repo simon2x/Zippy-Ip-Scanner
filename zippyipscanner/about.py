4d658ee440d2bd7de6c1cf76f35fa13e3eaeeae2
swprojects <swprojects@gmx.com>
2018-05-11 14:46:23 +0100

    Update py files to pass flake8 test

--- zippyipscanner/about.py
+++ zippyipscanner/about.py
@@ -22,29 +22,29 @@ import logging
 from version import __version__
 from license import __license__
 import sys
+from PyQt5.QtWidgets import (QApplication, QDialog, QGridLayout, QGroupBox,
+                             QLabel, QTextEdit, QHBoxLayout)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon, QKeyEvent
 
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-
-from portable import resource_path
 
 class AboutDialog(QDialog):
 
-    def __init__(self, parent=None):                    
+    def __init__(self, parent=None):
         super(AboutDialog, self).__init__(parent)
-        
+
         self.setWindowTitle("About Zippy Ip Scanner")
-        self.setWindowIcon(QtGui.QIcon(parent.appPath+"zippyipscanner.ico"))
+        if parent:
+            self.setWindowIcon(QIcon(parent.appPath + "zippyipscanner.ico"))
         self.initGUI()
         self.show()
-        
+
     def keyPressEvent(self, event):
         logging.info("AboutDialog->keyPressEvent")
-        if type(event) == QtGui.QKeyEvent:
-            if event.key() == QtCore.Qt.Key_Escape:
+        if type(event) == QKeyEvent:
+            if event.key() == Qt.Key_Escape:
                 self.close()
-            
+
     def initGUI(self):
         row = 0
         self.grid = QGridLayout()
@@ -52,63 +52,64 @@ class AboutDialog(QDialog):
         self.grid.setColumnStretch(1, 1)
         self.grid.setSpacing(5)
         self.setLayout(self.grid)
-        
+
         # About
-        aboutLayout = QGridLayout()  
-        gboxScan = QGroupBox("About", self) 
+        aboutLayout = QGridLayout()
+        gboxScan = QGroupBox("About", self)
         gboxScan.setLayout(aboutLayout)
         self.grid.addWidget(gboxScan, row, 0, 1, 2)
-        
+
         aboutLayout.addWidget(QLabel("Author:", self), 0, 0, 1, 2)
         authorLabel = QLabel("<a href=www.sanawu.com>www.sanawu.com</a>", self)
-        authorLabel.setTextFormat(QtCore.Qt.RichText);
-        authorLabel.setTextInteractionFlags(QtCore.Qt.TextBrowserInteraction)
+        authorLabel.setTextFormat(Qt.RichText)
+        authorLabel.setTextInteractionFlags(Qt.TextBrowserInteraction)
         aboutLayout.addWidget(authorLabel, 0, 2, 1, 2)
         authorLabel.setOpenExternalLinks(True)
-        
-        aboutLayout.addWidget(QLabel("Github:", self), 1, 0, 1, 2)     
+
+        aboutLayout.addWidget(QLabel("Github:", self), 1, 0, 1, 2)
         g = "https://github.com/swprojects/Zippy-Ip-Scanner"
-        homeLabel = QLabel ("<a href={0}>{1}</a>".format(g, g), self)
-        homeLabel.setTextFormat(QtCore.Qt.RichText);
-        homeLabel.setTextInteractionFlags(QtCore.Qt.TextBrowserInteraction)
+        homeLabel = QLabel("<a href={0}>{1}</a>".format(g, g), self)
+        homeLabel.setTextFormat(Qt.RichText)
+        homeLabel.setTextInteractionFlags(Qt.TextBrowserInteraction)
         homeLabel.setOpenExternalLinks(True)
         aboutLayout.addWidget(homeLabel, 1, 2, 1, 1)
-        
+
         aboutLayout.addWidget(QLabel("Version:", self), 2, 0, 1, 2)
         aboutLayout.addWidget(QLabel(str(__version__), self), 2, 2, 1, 2)
-        
-        
+
         # Mac Vender Lookup
         row += 1
         policyLayout = QHBoxLayout()
-        gboxPolicy = QGroupBox("Mac Vender Lookup", self) 
+        gboxPolicy = QGroupBox("Mac Vender Lookup", self)
         gboxPolicy.setLayout(policyLayout)
         self.grid.addWidget(gboxPolicy, row, 0, 1, 2)
-        
+
         pLink = "http://macvendors.co/kb/privacy-policy"
         p = ("Zippy Ip Scanner retrieves the MAC vendor name via MacVendors.co API and \n"
-            +"therefore MAC vendor name retrieval is subject to <a href={0}>MacVendors.co privacy policy</a>.\n".format(pLink)
-            +"Uncheck Manufacturer checkbox to disable this feature.")
+             + "therefore MAC vendor name retrieval is subject to <a href={0}>MacVendors.co"
+             + " privacy policy</a>.\n".format(pLink)
+             + "Uncheck Manufacturer checkbox to disable this feature.")
         policyLabel = QLabel(p, self)
         policyLabel.setWordWrap(True)
-        policyLabel.setTextFormat(QtCore.Qt.RichText)
-        policyLabel.setTextInteractionFlags(QtCore.Qt.TextBrowserInteraction)
+        policyLabel.setTextFormat(Qt.RichText)
+        policyLabel.setTextInteractionFlags(Qt.TextBrowserInteraction)
         policyLabel.setOpenExternalLinks(True)
         policyLayout.addWidget(policyLabel)
-                
+
         # License
         row += 1
         self.grid.setRowStretch(row, 1)
-        gboxScanLayout = QHBoxLayout()  
-        gboxScan = QGroupBox("License", self) 
+        gboxScanLayout = QHBoxLayout()
+        gboxScan = QGroupBox("License", self)
         gboxScan.setLayout(gboxScanLayout)
         self.grid.addWidget(gboxScan, row, 0, 1, 2)
         licenseText = QTextEdit()
         licenseText.setReadOnly(True)
-        licenseText.setAlignment(QtCore.Qt.AlignCenter)
+        licenseText.setAlignment(Qt.AlignCenter)
         licenseText.setText(__license__)
         gboxScanLayout.addWidget(licenseText)
-      
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     gui = AboutDialog()
--- zippyipscanner/functions.py
+++ zippyipscanner/functions.py
@@ -24,38 +24,39 @@ import re
 import urllib.request
 import socket
 import subprocess
-import time
-import threading
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSignal
+
 
 def GetHostByAddress(address, hostname):
     try:
         hostname.append(socket.gethostbyaddr(address)[0])
     except socket.herror:
         hostname.append("n/a")
-    
+
+
 class LookupHostname(QtCore.QThread):
-    
+
     signal = pyqtSignal(dict)
-    
+
     def __init__(self, parent, address, timeout):
         super(LookupHostname, self).__init__()
-        
+
         self.parent = parent
         self.timeout = timeout * 1000
         self.address = address
-        
+
         self.signal.connect(self.parent.receiveHostnameResult)
-                            
+
     def run(self):
         hostname = "n/a"
         try:
             hostname = socket.gethostbyaddr(self.address)[0]
         except socket.herror:
-            pass        
+            pass
         self.signal.emit({"address": self.address, "hostname": hostname})
-    
+
+
 def LookupMacAddress(address):
     """
     Finds the MAC Addresses using ARP
@@ -68,35 +69,36 @@ def LookupMacAddress(address):
         arp_cmd = ['arp', '-a']
     else:
         arp_cmd = ['arp', '-n']
-        
+
     info = None
     # Configure subprocess to hide the console window
     if os.name == 'nt':
         info = subprocess.STARTUPINFO()
         info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         info.wShowWindow = subprocess.SW_HIDE
-            
-    pid = subprocess.Popen(arp_cmd + [address], 
+
+    pid = subprocess.Popen(arp_cmd + [address],
                            stdin=subprocess.PIPE,
                            stderr=subprocess.PIPE,
                            stdout=subprocess.PIPE, startupinfo=info)
     out = pid.communicate()[0]
-    
+
     MAC_RE = re.compile(r'(([a-f\d]{1,2}[:-]){5}[a-f\d]{1,2})')
     mac_found = MAC_RE.search(out.decode('utf-8'))
-    
+
     if mac_found:
         mac = mac_found.group(0).replace('-', ':')
     else:
         mac = "n/a"
-        
-    logging.debug("address: %s, mac: %s" % (address, mac))    
-    
+
+    logging.debug("address: %s, mac: %s" % (address, mac))
+
     return mac
-    
+
+
 def LookupManufacturers(mac):
     """Request to macvendors.co for manufacturer name"""
-    macUrl = 'http://macvendors.co/api/{0}'.format(mac)    
+    macUrl = 'http://macvendors.co/api/{0}'.format(mac)
     req = urllib.request.Request(macUrl, headers={'User-Agent': 'Mozilla/5.0'})
     try:
         result = urllib.request.urlopen(req).read()
@@ -104,74 +106,74 @@ def LookupManufacturers(mac):
     except Exception as e:
         logging.debug("LookupManufacturers Exception: {0}".format(e))
         return ""
-    
+
     logging.debug("LookupManufacturers:Result: %s" % result)
     # logging.debug("request URL: %s" % r.text)
     result = json.loads(result)["result"]
-    
+
     try:
         mfn = result["company"]
         return mfn
-    except:
+    except Exception as e:
         return ""
+
 
 class PingAddress(QtCore.QThread):
 
     signal = pyqtSignal(dict)
     debugSignal = pyqtSignal(str)
-    
+
     def __init__(self, parent, addresses, scanParams):
         super(PingAddress, self).__init__()
-        
-        self.parent = parent 
+
+        self.parent = parent
         self.signal.connect(self.parent.receiveScanResult)
         self.debugSignal.connect(self.parent.receiveDebugSignal)
-        self.scanParams = scanParams   
+        self.scanParams = scanParams
         self._addresses = addresses
         self.start()
-        
+
     def run(self):
-        
+
         info = None
         # Configure subprocess to hide the console window
         if os.name == 'nt':
             info = subprocess.STARTUPINFO()
             info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
             info.wShowWindow = subprocess.SW_HIDE
-        
+
         for index, address in enumerate(self._addresses):
-        
+
             # For each IP address in the subnet, run the ping command
             address = str(address)
             if platform.system() == 'Windows':
                 cmd = ['ping', '-n', '1', '-w', '500', address]
             elif platform.system() == 'Linux':
                 cmd = ['ping', '-c', '1', '-w', '1', address]
-    
-            output = subprocess.Popen(cmd, 
+
+            output = subprocess.Popen(cmd,
                                       stdin=subprocess.PIPE,
                                       stderr=subprocess.PIPE,
                                       stdout=subprocess.PIPE,
                                       startupinfo=info).communicate()[0]
             output = output.decode('utf-8')
-            
+
             # parse TTL and response time (ms)
             ttl = ""
             ms = ""
             mac = ""
             mfn = ""
-            hostname = ""
             status = "Offline"
             if platform.system() == 'Windows':
                 if "Destination host unreachable" in output:
-                    pass                 
+                    pass
                 elif "Request timed out" in output:
                     if self.scanParams["MAC Address"] is True:
                         mac = LookupMacAddress(address)
-                        
+
                     if self.scanParams["Manufacturer"] is True:
                         mfn = LookupManufacturers(mac)
-                elif "General failure" in output: 
+                elif "General failure" in output:
                     pass
                 else:
                     self.debugSignal.emit("PingAddress->Output: %s" % output)
@@ -179,33 +181,33 @@ class PingAddress(QtCore.QThread):
                         status = "Online"
                         ttlStart = output.index("TTL=")
                         ttlEnd = output.index("\r\n\r\nPing statistics")
-                        ttl = output[ttlStart+len("TTL="):ttlEnd]
+                        ttl = output[ttlStart + len("TTL="):ttlEnd]
                         msStart = output.index("Average = ")
-                        ms = output[msStart+len(("Average = ")):].strip("\r\n")
-                    
-            if platform.system() == 'Linux':   
+                        ms = output[msStart + len(("Average = ")):].strip("\r\n")
+
+            if platform.system() == 'Linux':
                 if "ttl=" in output:
                     status = "Online"
                     ttlStart = output.index("ttl=")
                     ttlEnd = output.index(" time=")
-                    ttl = output[ttlStart+len("ttl="):ttlEnd]
+                    ttl = output[ttlStart + len("ttl="):ttlEnd]
                     msStart = output.index("time=")
-                    msEnd = output.index(" ms") 
-                    ms = output[msStart+len("time="):msEnd]
-          
+                    msEnd = output.index(" ms")
+                    ms = output[msStart + len("time="):msEnd]
+
             self.debugSignal.emit("PingAddress->Status: %s" % status)
             if status == "Online":
                 if self.scanParams["MAC Address"] == 2:
                     self.debugSignal.emit("PingAddress->MAC Address: %s" % address)
                     mac = LookupMacAddress(address)
-                
+
                 if self.scanParams["Manufacturer"] == 2:
                     self.debugSignal.emit("PingAddress->LookupManufacturers: %s" % mac)
                     mfn = LookupManufacturers(mac)
-                
+
                 # if self.scanParams["Hostname"] == 2:
                     # hostname = LookupHostname(address, self.scanParams["hostnameTimeout"])
-                    
+
             params = {}
             # params["index"] = index
             params["IP Address"] = address
@@ -215,7 +217,5 @@ class PingAddress(QtCore.QThread):
             params["MAC Address"] = mac
             # params["status"] = status
             # params["Hostname"] = hostname
-            
+
             self.signal.emit(params)
-        
-#end class PingAddress
